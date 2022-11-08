4c408248926630c5f685163a6bebbc85d02c8db4
Roman <romankos1993@gmail.com>
2022-11-08 22:45:54 +0300

    task 002 conpleted

---
+++ task002/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 55: Задайте двумерный массив.
+// Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Enter the number of rows array: ");
+    int rows = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter the number of columns array: ");
+    int columns = int.Parse(Console.ReadLine()!);
+    if (rows != columns)
+    {
+        Console.WriteLine("Do not replace rows with columns");
+    }
+    else
+    {
+        int[,] arr = GetArray(rows, columns);;
+        PrintArray(arr);
+        SwitchRowsAndColums(arr);
+    }
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void SwitchRowsAndColums(int[,] arr)
+{
+    int[,] newArr = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            newArr[i, j] = arr[j, i];
+        }
+
+    }
+    Console.WriteLine("New array:");
+    PrintArray(newArr);
+
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Main();
