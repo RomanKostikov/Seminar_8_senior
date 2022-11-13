1882de00399d7ff6e7c94c6c9744adb2f23dcd04
Roman <romankos1993@gmail.com>
2022-11-13 20:33:31 +0300

    homework 004(60) completed

---
+++ homework004(60)/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void Main()
+{
+    Console.Clear();
+    int[,,] array = new int[2, 2, 2];
+
+    FillArray(array);
+    PrintArray(array);
+}
+void FillArray(int[,,] arr)
+{
+    int value = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = value;
+                value++;
+            }
+
+        }
+
+    }
+}
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($" {arr[i, j, k]} ( {i}, {j}, {k})");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+Main();
